7defae2cd4f5ec9a41d8d6aa0d053036f9f5c64e
Klayton Prince <146005906+klaytonPrinceMS@users.noreply.github.com>
2024-09-24 12:54:48 -0300

    2024-09-24 12-54 Upando Noticias

---
+++ README.md
@@ -0,0 +1,45 @@
+# DTI
+
+***SECRETARIA MUNICIPAL DE ADMINISTRAÇÃO GERAL***
+
+**Departamento de Tecnologia da Informação - DTI**
+
+*Analista de Sistema Responsavel: PRINCE, K.B Matricula 010698*
+
+
+## Nome do projeto Principal:relogioDigital
+1. -> Noticias 
+   2. executavel jornal.exe
+
+
+## Descrição simplificada do projeto: xxxxx 
+1. Agregar noticias em uma pagina html
+
+
+## Distintivos:
+1. Metadados e testes dos softwares
+
+## Instalação:
+1. Funcionamento:
+   2. No pc Cliente 
+      3. Copiar a pasta Distribuicao para pc Cliente
+      3. Rodar o executavel jornal.exe e escolher a fonte de noticas desejada
+      3. Aguardar arquivo html ser criado e apresetado no navegador
+
+## Apoiar:
+Para informações e colaborações, entrar em contato com  
+1. -> E-mail: <dti@montesantodeminas.mg.gov.br>
+2. -> E-mail: <klayton.prince.ms@gmail.com>
+
+
+## Contribuindo:
+1. -> Projeto aberto
+
+## Autores e Agradecimentos:
+1. -> Analista de Sistema: PRINCE, K.B <klayton.prince.ms@gmail.com>
+
+## Licença:
+1. MIT - Permitido distribuição, utilização e modificação
+
+## Status do Projeto:
+1. Em andamento
---
+++ main.py
@@ -0,0 +1,87 @@
+from classesKBP import Sites, Noticias
+from urllib.parse import quote as converterUrl
+import os
+def menu():
+    '''
+    Função cria o menu que será exibido no terminal para o usuário
+    EXECUÇÂO:
+    Limpa tela
+    cria uma instância da classe Sites() com os links dos sites para ser usado no programa
+    cria uma instÂncia da classe Noticias()
+    Gera o menu e apos escolha faz a leitura das noticias solicitadas e gera um arquivo index.html e abre o arquivo no navegador padrão
+    :return: Sem retorno
+    '''
+    os.system('cls')
+    site = Sites()
+    noticias = Noticias()
+    print('''
+    ====================================================
+    ===          Agregador DTI - Noticias            ===
+    ====================================================
+    0  - Pesquisar
+    1  - Brasil
+    2  - Internacional
+    3  - Economia
+    4  - Saúde
+    5  - Ciências e Tecnologia
+    6  - Entretenimento
+    7  - Esporte
+    ''')
+    opcao = str(input('Qual opção de noticias:'))
+
+    match opcao:
+        case '0':
+            pesquisar = str(input("Qual tema vamos buscar: "))
+            print('''
+1 - Fonte Google News            
+            ''')
+            opcao1 = str(input("Qual base de dados usar: "))
+            match opcao1:
+                case '1':
+                    os.system('cls')
+                    noticias.get_noticiasGN(urlSite=fr'https://news.google.com/search?q={converterUrl(string=pesquisar)}&hl=pt-BR&gl=BR&ceid=BR%3Apt-419', tagPrincipal='.JtKRv')
+                    noticias.gerarSite(titulo=pesquisar)
+                    menu()
+        case '1': # Brasil
+            noticias.get_noticiasGN(urlSite=site.gn_brasil)
+            noticias.get_noticiasBBC(urlSite=site.bbc_brasil)
+            noticias.gerarSite(titulo='Brasil')
+            menu()
+        case '2': # Internacional
+            noticias.get_noticiasGN(urlSite=site.gn_internacional)
+            noticias.get_noticiasBBC(urlSite=site.bbc_internacional)
+            noticias.gerarSite(titulo='Internacional')
+            menu()
+        case '3': # economia
+            noticias.get_noticiasGN(urlSite=site.gn_economia)
+            noticias.get_noticiasBBC(urlSite=site.bbc_economia)
+            noticias.gerarSite(titulo='Economia')
+            menu()
+        case '4': # Saúde
+            noticias.get_noticiasGN(urlSite=site.gn_saude)
+            noticias.get_noticiasBBC(urlSite=site.bbc_saude)
+            noticias.gerarSite(titulo='Saúde')
+            menu()
+        case '5': # Ciencia e Tecnologia
+            noticias.get_noticiasGN(urlSite=site.gn_ciencia_tecnologia)
+            noticias.get_noticiasBBC(urlSite=site.bbc_ciencia)
+            noticias.gerarSite(titulo="Ciências e tecnologia")
+            menu()
+        case '6': # entretenimento
+            noticias.get_noticiasGN(urlSite=site.gn_entretenimento)
+            noticias.gerarSite(titulo='Entretenimento')
+            menu()
+        case '7': # esporte
+            noticias.get_noticiasGN(urlSite=site.gn_esporte)
+            noticias.gerarSite(titulo='Esporte')
+            menu()
+        case _:
+            print('Opção valida')
+            menu()
+#menu()
+
+site = Sites()
+noticia = Noticias()
+noticia.versao()
+
+
