cc988af9c4b97df61a7cb607cfea440c0afa7ed6
Klayton Prince <146005906+klaytonPrinceMS@users.noreply.github.com>
2024-10-15 09:29:30 -0300

    20241015 0929

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def menu():
     cria uma instÂncia da classe Noticias()
     Gera o menu e apos escolha faz a leitura das noticias solicitadas e gera um arquivo index.html e abre o arquivo no navegador padrão
     :return: Sem retorno
+    teste
     '''
     os.system('cls')
     site = Sites()
